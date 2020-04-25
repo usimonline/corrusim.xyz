e1d64983888540c3df6adc2d7d060d3c74d5aaa9
usimonline <111qwasZX>
2020-04-25 11:41:58 +0300

    1

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ require("head.php");
 <div class="price-table-wrapper">
 <div class="row">
 <div class="four columns price-item-wrapper wrapper mb0 best-price best-active">
-
+<div class="price-item" style="height: 50px;">
 <div class="price-title">Копирайтинг</div>
 <div class="price-tag"><?php echo $price_copy_1;?> руб.<span class="price-suffix">/1000 збп</span></div>
 <div class="price-content"></div>
@@ -75,7 +75,7 @@ require("head.php");
 </div>
 </div>
 <div class="four columns price-item-wrapper wrapper mb0 best-price best-active">
-
+<div class="price-item" style="height: 50px;">
 <div class="price-title">Корректура</div>
 <div class="price-tag"><?php echo $price_cor_1;?> руб.<span class="price-suffix">/1000 збп</span></div>
 <div class="price-content"></div>
@@ -83,7 +83,7 @@ require("head.php");
 </div>
 </div>
 <div class="four columns price-item-wrapper wrapper mb0 best-price best-active">
-
+<div class="price-item" style="height: 50px;">
 <div class="price-title">Редактура</div>
 <div class="price-tag"><?php echo $price_cor_3;?> руб.<span class="price-suffix">/1000 збп</span></div>
 <div class="price-content"></div>
