914d1b000f136f7c658427d22e97f5bf9d71f18b
usimonline <111qwasZX>
2020-04-26 18:13:38 +0300

    1

--- about.php
+++ about.php
@@ -39,7 +39,7 @@
                                 <div class="gdl-page-content">
                                     <h1 style="text-align: center; font-size: 22pt;">Отзывы</h1>
 
-                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв главного редактора портала otzyvmarketing.ru:</h2>
+                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв главного редактора портала otzyvmarketing.ru Виктории Самиры:</h2>
 <blockquote style="font-size: 18pt;"><span style="color: #005EA4;">"Очень удобно с вами работать. За вами не нужно ничего редактировать. Вы сдаете отличные тексты и вовремя.
     Работаем с вами уже больше года - и всем довольны."</span></blockquote>
  <p style="font-size: 16pt;">Мой комментарий: я работаю автором-экспертом портала otzyvmarketing.ru 15 месяцев. Пишу экспертные статьи от 7 тыс. знаков без пробелов, отвечаю на вопросы
@@ -49,7 +49,7 @@
     <li><span style="font-size: 14pt;">рост трафика сайта на 16%;</span></li>
     <li><span style="font-size: 14pt;">сайт otzyvmarketing.ru поднялся на первое место в рейтинге "Яндекс.Кью" в рубрике "Маркетинг".</span></li>
 </ul>
-                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв главного редактора сайта dirsalona.ru:</h2>
+                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв главного редактора сайта dirsalona.ru Алисы Ануфриевой:</h2>
                                     <blockquote style="font-size: 18pt;"><span style="color: #005EA4;">"Ольга, вы даете отличные материалы. Ваши статьи очень приятно читать.
                                             Всегда соблюдаете указанный в ТЗ уровень уникальности, тошноты и не забываете про ключевые слова. Спасибо!"</span></blockquote>
                                     <p style="font-size: 16pt;">Мой комментарий: уже 7 месяцев я сотрудничаю с редакцией сайта dirsalona.ru.
@@ -61,7 +61,7 @@
                                         <li><span style="font-size: 14pt;">12 статей входят в топ-3 выдачи Google.</span></li>
                                     </ul>
 
-                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв владельца vostok.fitness с сайта fl.ru:</h2>
+                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв владельца сайта vostok.fitness Тимура Хакимова:</h2>
                                     <blockquote style="font-size: 18pt;"><span style="color: #005EA4;">"Ольга показала себя настоящим профессионалом.
                                             Остались очень довольны работой. Продолжаем сотрудничество. Рекомендуем всем!"</span></blockquote>
                                     <p style="font-size: 16pt;">Мой комментарий: выполняю обязанности главного редактора и автора сайта vostok.fitness 11 месяцев.
@@ -73,7 +73,7 @@
                                         <li><span style="font-size: 14pt;">рост посещаемости сайта на 80% за месяц.</span></li>
                                     </ul>
 
-                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв сооснователя онлайн-школы "Петь Легко":</h2>
+                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв сооснователя онлайн-школы "Петь Легко" Жанны Серопян:</h2>
                                     <blockquote style="font-size: 18pt;"><span style="color: #005EA4;">"Ольга работает усердно и никогда не задерживает работу.
                                             На нее можно положиться. У нее отличный слог, она разбирается в том, как нужно оформить статью. Выделяет где нужно цитаты,
                                             выделяет жирным ключевые предложения. Ее текст всегда уникальный. Очень довольные сотрудничеством с ней."</span></blockquote>
@@ -87,11 +87,14 @@
                                         <li><span style="font-size: 14pt;">увеличение конверсии после работы над сайтом на 23%.</span></li>
                                     </ul>
 
-
-
-
-
-
+                                    <h2 style="text-align: center; font-size: 18pt;">Отзыв главного редактора информационного агентства по ХМАО-Югре muksun.fm Юлии Глазовой:</h2>
+                                    <blockquote style="font-size: 18pt;"><span style="color: #005EA4;">"Я уважаю вас как профессионала, а также вашу позицию.
+                                            Было очень приятно с вами работать."</span></blockquote>
+                                    <p style="font-size: 16pt;">Мой комментарий: я писала новости для информационного агентства. Пришлось расстаться, поскольку редакционная политика
+                                        не соответствовала моим политическим убеждениям. Публиковала до 10 новостей по будним дням. Отслеживала информационные поводы.
+                                        Работала с "Яндекс.Новостями" и
+                                        использовала Google Allert.
+                                    </p>
 
 <div class="clear"></div>
 <div class="gdl-divider gdl-border-x top">
